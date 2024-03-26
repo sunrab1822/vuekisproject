30ef73d11387f68f6420c886d69dc3635eb1e904
sunrab1822 <wbarnus99@gmail.com>
2024-03-26 11:09:18 +0100

    asd,

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import DataService from "../services/dataservice";
+import { ref, onMounted } from "vue";
+  const products = ref([])
+  DataService.getAllProducts().then(resp => {
+          
+                products.value = resp.data
+                
+            })
+            .catch(err =>{
+               console.log(err);
+               
+            })
+
+</script>
+
+<template>
+  <select name="" id="">
+    <option v-for="product in products" value="">{{ product.manufacturer_id }}</option>
+  </select>
+  
+</template>
